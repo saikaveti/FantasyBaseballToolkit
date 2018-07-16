09b075dc34e4d98ce81b36fbc162337a281c5487
saikaveti <saileshkaveti@hotmail.com>
2018-07-15 23:02:53 -0400

    Main File Changed

--- main.py
+++ main.py
@@ -8,19 +8,22 @@ stats_file = "stats_file.txt"
 
 type_analysis = "similar"
 
+first_name = "paul"
+last_name = "goldschmidt"
+
 ### TO REDUCE TYPING WHILE TESTING
 
 ###first_name = raw_input("First Name: ")
 ###last_name = raw_input("Last Name: ")
 
 if type_analysis == "range":
-    main_stats = StatsRange("mike", "trout", stats_file)
+    main_stats = StatsRange(first_name, last_name, stats_file)
     main_stats.driver()
 
 if type_analysis == "comparison":
-    compare_stats = PlayerComparison(["j.d.", "mike"], ["martinez", "trout"], 14, stats_file, "", "")
+    compare_stats = PlayerComparison(["j.d.", first_name], ["martinez", last_name], 14, stats_file, "", "")
     compare_stats.tabulate_players(True)
 
 if type_analysis == "similar":
-    similarPlayers = SimilarPlayerSearch("mike", "trout", 14, stats_file)
+    similarPlayers = SimilarPlayerSearch(first_name, last_name, 14, stats_file)
     similarPlayers.find_similar_players(7)
