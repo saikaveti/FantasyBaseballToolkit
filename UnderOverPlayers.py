cf51309f46b795db4cafc651577fff4cdf15e459
saikaveti <saileshkaveti@hotmail.com>
2018-07-30 23:44:31 -0400

    clustering working

--- UnderOverPlayers.py
+++ UnderOverPlayers.py
@@ -42,25 +42,69 @@ class UnderValuedPlayers:
 
         obj = pd.read_csv("raw_data.csv")
 
-        print(obj)
+        X = pd.DataFrame(obj)
+
+        X = X.drop(columns = ['OBP', 'SLG', 'TA', 'SECA', 'FIRST_NAME', "LAST_NAME", "RC27"]).astype(float)
+
+        num = 5
+
+        clf = KMeans(n_clusters = num)
+        clf.fit(X)
+
+        cluster_map = pd.DataFrame()
+        cluster_map['data_index'] = X.index.values
+        cluster_map['cluster'] = clf.labels_
+
+        complete_underrated = list()
 
 
-        X = obj.ix[2, 5]
+        for i in range(num):
+            mapping = cluster_map[cluster_map.cluster == i]
+            list_indexes = list()
 
-        kmeans=KMeans(n_clusters=5, init='k-means++', max_iter= 300, n_init= 10, random_state= 0)
-        kmeans.fit(X)
+            for index, row in mapping.iterrows():
+                list_indexes.append(row['data_index'])
 
-        #df.drop(['OBP', 'SLG', 'TA', 'SECA'])
+            total_values = X.values.tolist()
+
+            list_cluster = list()
+
+            for index in list_indexes:
+                temp_value = total_values[index]
+
+                for player in list_players:
+                    if temp_value[0] == player.BA and temp_value[1] == player.OPS and temp_value[2] == player.ISO:
+                        list_cluster.append(player)
 
 
+            size = len(list_cluster)
+
+            total_RC27 = 0.0
+
+            for player in list_cluster:
+                total_RC27 += player.RC27
+
+            average_RC27 = total_RC27 / size
+
+            for player in list_cluster:
+                if player.RC27 < average_RC27:
+
+                    if (average_RC27 - player.RC27) / average_RC27 > .1:
+                        complete_underrated.append(player)
+
+        return complete_underrated
 
     def tabulate_under_players(self):
         total_players = self.get_players()
-        babip_players = self.prune_on_BABIP(total_players)
 
-        self.prune_on_clustering(total_players)
+        babip_players = self.prune_on_BABIP(total_players)
+        clustering_players = self.prune_on_clustering(total_players)
 
         final_list = babip_players
+
+        for player in clustering_players:
+            final_list.append(player)
+
 
         first_name_list = list()
         last_name_list = list()
