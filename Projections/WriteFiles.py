d8f236f8189ad488cc5e7cd6010a6ad70806e951
saikaveti <saileshkaveti@hotmail.com>
2018-08-29 18:42:14 -0400

    starting write files

---
+++ Projections/WriteFiles.py
@@ -0,0 +1,20 @@
+from pybaseball import batting_stats_range
+from tabulate import tabulate
+
+class WriteData:
+
+    def __init__(self, year):
+		self.year = year
+
+    def write_data_to_text(self, year):
+        file = open("", 'w')
+
+        data = batting_stats_range(prev_date, last_date)
+
+        for i, row in data.iterrows():
+            file.write(str(row))
+            file.write("\n")
+
+        file.close()
+
+        return data
