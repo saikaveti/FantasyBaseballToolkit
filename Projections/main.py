525b72e1aed0ae8dfff043c23a66bead3432ad7d
saikaveti <saileshkaveti@hotmail.com>
2018-09-03 15:51:41 -0400

    all csv files written

--- Projections/main.py
+++ Projections/main.py
@@ -7,3 +7,15 @@ reader = ReadFile("stats_file.txt")
 writer.write_data_to_text(3)
 players = reader.generate_players()
 writer.write_clean_data_to_csv(players, "threeyears.csv")
+
+writer.write_data_to_text(2)
+players = reader.generate_players()
+writer.write_clean_data_to_csv(players, "twoyears.csv")
+
+writer.write_data_to_text(1)
+players = reader.generate_players()
+writer.write_clean_data_to_csv(players, "oneyear.csv")
+
+writer.write_data_to_text(0)
+players = reader.generate_players()
+writer.write_clean_data_to_csv(players, "currentyear.csv")
